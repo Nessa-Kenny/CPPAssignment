6ba859804fb774b9c7683110452c91b08c24430d
Nessa-Kenny <nessakenny@gmail.com>
2020-11-29 23:57:21 +0000

    CA recipe v2

--- recipe_log/recipe_journal/urls.py
+++ recipe_log/recipe_journal/urls.py
@@ -3,9 +3,14 @@ from django.urls import path
 from .import views
 from django.urls import include
 from django.views.generic import RedirectView
+from .views import LoginView, ProfileView, SearchView, Add_RecipeView
 
 app_name='recipe_journal'
 urlpatterns = [
     #homepage
     path('',views.index, name='index'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('profile/', ProfileView.as_view(), name='profile'),
+    path('search/', SearchView.as_view(), name='search'),
+    path('add_recipe/', Add_RecipeView.as_view(), name='add_recipe'),
 ]
--- recipe_log/recipe_journal/views.py
+++ recipe_log/recipe_journal/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
-from django.shortcuts import render
-
 def index(request):
     """The home page for recipe_journal"""
     return render(request,'recipe_journal/index.html')
+
+class LoginView(TemplateView):
+    template_name ='recipe_journal/login.html'
     
-#class LoginView(generic.Detail.view):
-#    model = Entry
-#   template_name ='recipe_journal/login.html'
+class ProfileView(TemplateView):
+    template_name ='recipe_journal/profile.html'
+    
+class SearchView(TemplateView):
+    template_name ='recipe_journal/search.html'
+    
+class Add_RecipeView(TemplateView):
+    template_name ='recipe_journal/add_recipe.html'
